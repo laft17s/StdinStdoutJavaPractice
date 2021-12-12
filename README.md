d14721f68d51f9594618470aa39e18411ef3bdd8
Sean17nl <sean17nl@gmail.com>
2021-12-11 22:11:33 -0500

    finish project

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Stdin & Stdout Practice
 
-
 ## Excercise
 
 Receive a String and an Integer by keyboard and then print it.
@@ -8,12 +7,14 @@ Receive a String and an Integer by keyboard and then print it.
 ## Example
 
 ### Input
+
 > Hi<br/>
 > 5
 
 <br/>
 
 ### Output
+
 > myString is: <b>Hi</b><br/>
 > myInt is: <b>5</b>
 
--- src/App.java
+++ src/App.java
@@ -3,6 +3,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        /**
+         * @title: Stdin & Stdout Practice
+         * 
+         *         ## Excersise:
+         *         Receive a String and an Integer by keyboard and
+         *         then print it.
+         * 
+         *         ## Example:
+         * 
+         *         ### Input
+         *         >> Hi
+         *         >> 5
+         * 
+         *         ### Output
+         *         >> myString is: Hi
+         *         >> myInt is: 5
+         */
+
         // Creamos una instancia de la clase Scanner
         Scanner scan = new Scanner(System.in);
 
