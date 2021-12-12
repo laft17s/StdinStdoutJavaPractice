d8a3d074d8e7d4a4d5d36b8c5a7161a9af1320a0
Sean17nl <sean17nl@gmail.com>
2021-12-11 22:03:55 -0500

    solution

--- src/App.java
+++ src/App.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        
+        // Creamos una instancia de la clase Scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Ingreso por teclado de una cadena de caracteres
+        System.out.println("Insert a word...");
+        String word = scan.next();
+
+        // Ingreso por teclado de un entero
+        System.out.println("Insert a integer...");
+        int number = scan.nextInt();
+
+        // Cierre de objeto scan
+        scan.close();
+
+        // Imprimir variables
+        System.out.println("myString is: " + word + "\nmyInt is: " + number);
 
     }
 }
